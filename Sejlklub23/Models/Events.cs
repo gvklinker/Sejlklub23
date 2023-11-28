9c2d049a2b15dc7b2d171ef767203322023142cb
Stéphane Karlsen <stk002@edu.zealand.dk>
2023-11-28 11:00:53 +0100

    Events.cs

--- Sejlklub23/Models/Events.cs
+++ Sejlklub23/Models/Events.cs
@@ -1,6 +1,29 @@
-﻿namespace Sejlklub23.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sejlklub23.Models
 {
     public class Events
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime StartOfEvent { get; set;}
+        [Required]
+        public int EventDuration { get; set;}
+        [Required]
+        public string Address { get; set; }
+
+        public Events (int id, string title, string description, DateTime startOfEvent, int eventDuration, string address)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            StartOfEvent = startOfEvent;
+            EventDuration = eventDuration;
+            Address = address;
+        }
     }
 }
