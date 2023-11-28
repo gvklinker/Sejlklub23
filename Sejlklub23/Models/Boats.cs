d0ac3884e40344fb96fc61564901c319473e0084
Stéphane Karlsen <stk002@edu.zealand.dk>
2023-11-28 12:33:59 +0100

    Boats.cs

--- Sejlklub23/Models/Boats.cs
+++ Sejlklub23/Models/Boats.cs
@@ -1,6 +1,20 @@
-﻿namespace Sejlklub23.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sejlklub23.Models
 {
     public class Boats
     {
+        public int Id { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public Boats (int id, string model, string name)
+        {
+            Id = id;
+            Model = model;
+            Name = name;
+        }
     }
 }
