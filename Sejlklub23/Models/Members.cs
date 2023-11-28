045144422ab1c6545f36206526e426e215834306
Stéphane Karlsen <stk002@edu.zealand.dk>
2023-11-28 10:46:21 +0100

    Members

--- Sejlklub23/Models/Members.cs
+++ Sejlklub23/Models/Members.cs
@@ -1,6 +1,32 @@
-﻿namespace Sejlklub23.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sejlklub23.Models
 {
     public class Members
     {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public Members(int id, string name, string password, string email, string phoneNumber, string address) 
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            IsAdmin = false;
+        }
     }
 }
