e0f6b7ef89a380feb6dee54121fa42c13eb71ce3
Stéphane Karlsen <stk002@edu.zealand.dk>
2023-12-04 16:35:02 +0100

    delEvent.cshtml

--- Sejlklub23/Pages/Events/DeleteEvent.cshtml.cs
+++ Sejlklub23/Pages/Events/DeleteEvent.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sejlklub23.Interfaces;
+using Sejlklub23.Models;
 
 namespace Sejlklub23.Pages.Events
 {
     public class DeleteEventModel : PageModel
     {
-        public void OnGet()
+        private IEventRepository _repo;
+
+        //[BindProperty]
+        public Event DeleteEvent { get; set; }
+
+
+        public DeleteEventModel(IEventRepository repo)
         {
+            _repo = repo;
+        }
+        public IActionResult OnGet(int deleteId)
+        {
+            DeleteEvent = _repo.GetEvent(deleteId);
+            return Page();
+        }
+
+        public IActionResult OnPostDelete(int nummer)
+        {
+            DeleteEvent = _repo.GetEvent(nummer);
+            _repo.DeleteEvent(DeleteEvent);
+            return RedirectToPage("Index");
         }
     }
 }
