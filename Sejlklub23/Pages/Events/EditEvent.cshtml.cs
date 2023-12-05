3de1f4c922f4508071a9bc28aaddfa1738f2b7a8
Stéphane Karlsen <stk002@edu.zealand.dk>
2023-12-05 13:02:16 +0100

    EditEvent

--- Sejlklub23/Pages/Events/EditEvent.cshtml.cs
+++ Sejlklub23/Pages/Events/EditEvent.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sejlklub23.Interfaces;
+using Sejlklub23.Models;
 
 namespace Sejlklub23.Pages.Events
 {
     public class EditEventModel : PageModel
     {
-        public void OnGet()
+        private IEventRepository _eventRepo;
+
+        [BindProperty]
+        public Event EventToUpdate { get; set; }
+
+        public EditEventModel(IEventRepository eventRepository)
         {
+
+            _eventRepo = eventRepository;
+
         }
+        public void OnGet(int id)
+        {
+            EventToUpdate = _eventRepo.GetEvent(id);
+        }
+
+        public IActionResult OnPostUpdate()
+        {
+            _eventRepo.UpdateEvent(EventToUpdate);
+            return RedirectToPage("Index");
+        }
+
     }
 }
