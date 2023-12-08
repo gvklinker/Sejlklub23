6e1e5a67b91cce5645aef2160b19b548a0a650b0
Stéphane Karlsen <stk002@edu.zealand.dk>
2023-12-08 11:48:50 +0100

    loginsystem

---
+++ Sejlklub23/Pages/Members/LoginSystem.cshtml.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sejlklub23.Interfaces;
+using Sejlklub23.Models;
+
+namespace Sejlklub23.Pages.Members
+{
+    public class LoginSystemModel : PageModel
+    {
+        [BindProperty] public string MemberName { get; set; }
+        [BindProperty] public string Password { get; set; }
+
+        public string Message { get; set; }
+
+
+
+        private IMemberRepository _memberService;
+        public LoginSystemModel(IMemberRepository Memberservice)
+        {
+            _memberService = Memberservice;
+        }
+
+        public void OnGet()
+        {
+        }
+        public void OnGetLogout()
+        {
+            HttpContext.Session.Remove("MemberName");
+        }
+
+        public IActionResult OnPost()
+        {
+            Member loginMember = _memberService.VerifyMember(MemberName, Password);
+            if (loginMember != null)
+            {
+                HttpContext.Session.SetString("MemberName", loginMember.Password);
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                Message = "Invalid Membername or password";
+                MemberName = "";
+                Password = "";
+                return Page();
+            }
+        }
+    }
+}
---
+++ Sejlklub23/Services/LoginService.cs
@@ -0,0 +1,22 @@
+﻿using Sejlklub23.Models;
+
+namespace LoginSessionTest.Services
+{
+    public class LogInService
+    {
+        private Member _loggedInMember;
+
+        public void MemberLogIn(Member member)
+        {
+            _loggedInMember = member;
+        }
+        public void MemberLogOut()
+        {
+            _loggedInMember = null;
+        }
+        public Member GetLoggedMember()
+        {
+            return _loggedInMember;
+        }
+    }
+}
